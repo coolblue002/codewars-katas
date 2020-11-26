47eaed0ab3fac60d82365995e6dedaa84e10e92e
Mircea Nonea <mirceanonea@gmail.com>
2020-11-26 16:18:50 +0000

    One kata done
    Final multiplication-tabel-for-numbers.js

--- multiplication-table-for-number.js
+++ multiplication-table-for-number.js
@@ -1,11 +1,13 @@
+// https://www.codewars.com/kata/5a2fd38b55519ed98f0000ce/train/javascript
+
 function multiTable(number) {
   // good luck
   let table = '';
   for (let i = 1; i <= 10; i++){
     if (i !== 10) {
-      table = table + i + ' * ' + number + ' = '  i * number + '\n';
+      table += i.toString() + ' * ' + number.toString() + ' = ' + (i * number).toString() + '\n';
     } else {
-      table = table + i + ' * ' + number + ' = '  i * number;
+      table += i.toString() + ' * ' + number.toString() + ' = ' + (i * number).toString();
     }
   }
   return table;

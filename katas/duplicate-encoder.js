e603e83eea97426bc6d95e0224dd2ff0d434fb8c
Mircea Nonea <mirceanonea@gmail.com>
2020-11-26 16:38:17 +0000

    started new kata
    Started duplicate encoder kata

---
+++ katas/duplicate-encoder.js
@@ -0,0 +1,18 @@
+// https://www.codewars.com/kata/54b42f9314d9229fd6000d9c/train/javascript
+
+
+// console.log('muie psd');
+
+function duplicateEncode(word){
+  // ...
+  //get if each letter is once or more times in the word
+  individualLetterCounter = [];
+  for (let i = 0; i < word.length; i++) {
+    console.log(i);
+  }
+
+  //create new word with the proper encoding
+
+}
+
+duplicateEncode('Success');

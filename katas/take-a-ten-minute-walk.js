eaa12c13db6897971e5d80b1a837c9a3d934dd84
Mircea Nonea <mirceanonea@gmail.com>
2020-12-03 17:34:59 +0000

    finished a kata
    Finished take-a-ten-minute-walk.js

--- katas/take-a-ten-minute-walk.js
+++ katas/take-a-ten-minute-walk.js
@@ -1,3 +1,23 @@
 // https://www.codewars.com/kata/54da539698b8a2ad76000228/train/javascript
 
-console.log('test');
+function isValidWalk(walk) {
+  //insert brilliant code here
+  let total = {};
+  total.n = 0;
+  total.s = 0;
+  total.e = 0;
+  total.w = 0;
+
+  for (let i = 0; i < walk.length; i++) {
+    total[walk[i]]++;
+  }
+
+  console.log(total);
+  if (total.n === total.s && total.e === total.w && walk.length === 10) {
+    return true;
+  }
+
+  return false;
+}
+
+console.log(isValidWalk(['n', 's', 'n', 's', 'n', 's', 'n', 's', 'n', 's']));

81b225dc8235eaec61981796e741532900bf7385
Mircea Nonea <mirceanonea@gmail.com>
2020-12-09 12:14:47 +0000

    finished a Kata
    finished is-a-prime-number.js

---
+++ katas/is-a-number-prime.js
@@ -0,0 +1,19 @@
+// https://www.codewars.com/kata/5262119038c0985a5b00029f/train/javascript
+
+function isPrime(num) {
+  //TODO
+  if (Number.isInteger(Math.sqrt(num))) {
+    return false;
+  }
+  if (num <= 0) {
+    return false;
+  }
+  for (let i = 2; i < Math.sqrt(num); i++) {
+    if (Number.isInteger(num / i)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(isPrime(8));

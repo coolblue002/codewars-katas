3e9120665dcc8e7bf4f56389b64953d86bb810a1
Mircea Nonea <mirceanonea@gmail.com>
2020-12-11 17:07:54 +0000

    finished stop-gninnips-my-sdrow.js with tests

--- katas/stop-gninnips-my-sdrow.js
+++ katas/stop-gninnips-my-sdrow.js
@@ -1,11 +1,22 @@
 // stop-gninnips-my-sdrow.js
 // https://www.codewars.com/kata/5264d2b162488dc400000001/train/javascript
 
-function spinWords(){
+function spinWords(words) {
   //TODO Have fun :)
-  // return 'Hey wollef sroirraw';
-  
-
+  let wordsArray = words.split(' ');
+  let inversedWord = '';
+  for (let i = 0; i < wordsArray.length; i++) {
+    if (wordsArray[i].length > 4) {
+      inversedWord = '';
+      for (let j = wordsArray[i].length - 1; j >= 0; j--) {
+        inversedWord += wordsArray[i][j];
+      }
+      wordsArray[i] = inversedWord;
+    }
+  }
+  return wordsArray.join(' ');
 }
+
+// console.log(spinWords('Hey fellow warriors'));
 
 module.exports = spinWords;

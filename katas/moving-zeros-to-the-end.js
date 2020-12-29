064939c499e2c924e20e876973234907863eddf6
Mircea Nonea <mirceanonea@gmail.com>
2020-12-29 21:14:24 +0000

    working on moving zeros
    it's going into an infinite loop and I don't know why

--- katas/moving-zeros-to-the-end.js
+++ katas/moving-zeros-to-the-end.js
@@ -2,21 +2,21 @@
 
 var moveZeros = function (arr) {
   // TODO: Program me
-  let necessaryLength = arr.length;
-  // console.log(necessaryLength);
-  for(let i = 0; i < necessaryLength; i++) {
-    console.log(arr);
-    console.log(i);
+  for(let i = 0; i < arr.length; i++) {
     if (arr[i] === 0) {
-      console.log('acu');
-      arr.push(0);
-      arr.splice(i, 1);
+      for (let j = i; j <arr.length - 1; j++) {
+        arr[j] = arr[j + 1];
+      }
+      arr[arr.length - 1] = 0;
       i = i - 1;
-    } 
+    }
   }
   return arr;
 }
 
-console.log(moveZeros([false,1,0,1,2,0,1,3,"a"]));
+// console.log(moveZeros([false,1,0,1,2,0,1,3,"a"]));
+console.log(moveZeros([9,0,0,9,1,2,1,1,3,1,9,0,0,9,0,0,0,0,0,0,0]));
+console.log(moveZeros(["a",0,"b","c","d",1,1,3,1,9,0,0,9,0,0,0,0,0,0,0]));
+console.log(moveZeros(["a",0,"b",null,"c","d",1,false,1,3,[],1,9,0,{},0,9,0,0,0,0,0,0,0]));
 
 module.exports = moveZeros;

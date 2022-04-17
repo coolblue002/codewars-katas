79fa5de23920d27f87fe8f0314c44e409524fc13
Mircea Nonea <57224243+coolblue002@users.noreply.github.com>
2022-04-18 00:32:26 +0300

    where my anagrams solutions plus new kata

---
+++ katas/my-smallest-code-interpreter.js
@@ -0,0 +1,3 @@
+// https://www.codewars.com/kata/526156943dfe7ce06200063e/train/javascript
+
+console.log('test');
--- katas/where-my-anagrams-at.js
+++ katas/where-my-anagrams-at.js
@@ -1,20 +1,33 @@
 
+//https://www.codewars.com/kata/523a86aa4230ebb5420001e1/train/javascript
+
+// function anagrams(word, words) {
+//   let newWord = word.split('').sort();
+//   let result = newWord.filter((element, index) => index === newWord.indexOf(element));
+//   // console.log('the word array is ' + result);
+//   let placeholder =[];
+//   let placeholerResult = '';
+//   let final = words.filter(element => {
+//     placeholder = element.split('').sort();
+//     placeholerResult = placeholder.filter((element, index) => index === placeholder.indexOf(element));
+//     // console.log('the array for element ' + element + ' is ' + placeholder);
+//     return placeholerResult.toString() === result.toString();
+//   })
+//     // console.log(final);
+//     console.log(final);
+// }
+
+let gigel = function (y) {
+  return y.split("").sort().join("");
+};
+
 function anagrams(word, words) {
-  let newWord = word.split('').sort();
-  let result = newWord.filter((element, index) => index === newWord.indexOf(element));
-  console.log(result);
-  let placeholder =[];
-  let placeholerResult = '';
-  let final = words.filter(element => {
-    placeholder = element.split('').sort();
-    placeholerResult = placeholder.filter((element, index) => index === newWord.indexOf(element));
-    console.log(placeholerResult);
-    return placeholerResult.toString() === result.toString();
-  })
-    console.log(final);
+  return words.filter(function(x) {
+      return gigel(x) === gigel(word);
+  });
 }
 
-let word = 'racer';
-let words = ['crazer', 'carer', 'racar', 'caers', 'racer'];
+let word = 'laser';
+let words = ['lazing', 'lazy',  'lacer'];
 
 anagrams(word, words);
